a73da0716520c72283ac98d76b4574624c022ded
Snevy1 <simiyunevily@gmail.com>
2025-10-24 15:49:48 +0300

    updated server.ts

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,18 @@
-export default ({ env }) => ({
+/* export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+}); */
+
+
+export default ({ env }) => ({
+  url: env('PUBLIC_URL', 'https://pleasant-miracle-31b4db12d1.strapiapp.com'),
+  host: '0.0.0.0',
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
+
+ 
