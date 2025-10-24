1b52bd055965e11fbecfcffe2096e0b0a99784af
Snevy1 <simiyunevily@gmail.com>
2025-10-24 15:14:17 +0300

    Updated nextConfig to reflect place of media properly

--- client/next.config.ts
+++ client/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       {
         protocol: process.env.NODE_ENV === "development" ? "http" : "https",
         hostname: STRAPI_HOSTNAME,
-        pathname: "/uploads/**/*",
+        pathname: "/**",
       },
     ],
   },
